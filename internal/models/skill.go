20babdc71ef2fe22dcd657e67f16bda461712aa1
James John James <jamesjay4199@gmail.com>
2023-10-12 14:29:26 +0100

    singular table name for userAssessment

--- internal/models/skill.go
+++ internal/models/skill.go
@@ -46,17 +46,21 @@ func (a Assessment) TableName() string {
 }
 
 type UserAssessment struct {
-	ID              uint      `json:"id" gorm:"primaryKey"`
-	UserID         	string    `json:"user_id"`
-	AssessmentID    uint      `json:"assessment_id"`
-	Score       	uint      `json:"score"`
-	TimeSpent 		uint      `json:"time_spent"`
-	SubmissionDate  time.Time `json:"submisssion_date"`
-	Status          Status    `json:"status"`
-	CreatedAt       time.Time `json:"created_at"`
-	UpdatedAt       time.Time `json:"updated_at"`
+	ID             uint      `json:"id" gorm:"primaryKey"`
+	UserID         string    `json:"user_id"`
+	AssessmentID   uint      `json:"assessment_id"`
+	Score          uint      `json:"score"`
+	TimeSpent      uint      `json:"time_spent"`
+	SubmissionDate time.Time `json:"submisssion_date"`
+	Status         Status    `json:"status"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 
 	Assessment Assessment
+}
+
+func (uA UserAssessment) TableName() string {
+	return "user_assessment"
 }
 
 func FindSkillById(db *gorm.DB, skillID uint) (*Skill, error) {
