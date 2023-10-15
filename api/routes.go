81a4e9d06d28372836540f91f4658580decccaa1
Daniel Promise <promise.olayimika.171076@fuoye.edu.ng>
2023-10-15 17:44:44 +0100

    patch:view badge user auth

--- api/routes.go
+++ api/routes.go
@@ -25,10 +25,10 @@ func SetupRoutes() *gin.Engine {
 	// All other API routes should be mounted on this route group
 	apiRoutes := r.Group("/api")
 	apiRoutes.POST("/badges", handlers.CreateBadgeHandler)
-	apiRoutes.GET("/user/:user_id/badges", handlers.GetBadgesForUserHandler)
+	apiRoutes.GET("/user/:user_id/badges", middleware.CanViewBadge(), handlers.GetBadgesForUserHandler)
 	apiRoutes.POST("/user/badges", middleware.CanAssignBadge(), handlers.AssignBadgeHandler)
-	apiRoutes.GET("/user/badges/:userId/skill/:skillId", handlers.GetUserBadgeHandler)
-	apiRoutes.GET("/badges/:badge_id", handlers.GetUserBadgeByIDHandler)
+	apiRoutes.GET("/user/badges/:userId/skill/:skillId", middleware.CanViewBadge(), handlers.GetUserBadgeHandler)
+	apiRoutes.GET("/badges/:badge_id", middleware.CanViewBadge(), handlers.GetUserBadgeByIDHandler)
 
 	return r
 }
